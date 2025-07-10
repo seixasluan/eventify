d7af328bdb30bb9bb1ea00b7a83c736cd27b6f38
Luan Seixas <mari04luan@gmail.com>
2025-07-10 20:38:59 -0300

    feat: list events created by organizer

--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -5,13 +5,19 @@ import {
   updateEventHandler,
   deleteEventHandler,
   listPublicEventsHandler,
+  listOrganizerEventsHandler,
 } from "../controllers/eventController";
 import { authenticate } from "../middleware/auth";
 import { authorizeOrganizer } from "../middleware/authorizeOrganizer";
 
 export async function eventRoutes(fastify: FastifyInstance) {
-  fastify.get("/events/:id", { preHandler: [authenticate] }, getEventHandler);
   fastify.get("/events", listPublicEventsHandler);
+  fastify.get(
+    "/events/mine",
+    { preHandler: [authenticate, authorizeOrganizer] },
+    listOrganizerEventsHandler
+  );
+  fastify.get("/events/:id", { preHandler: [authenticate] }, getEventHandler);
   fastify.post(
     "/events",
     {
